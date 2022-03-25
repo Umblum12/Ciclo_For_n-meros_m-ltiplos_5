3412d6bd4649ec8c9908fee282672c875f47bd5c
Umblum12 <68798247+Umblum12@users.noreply.github.com>
2022-03-25 03:25:13 -0600

    Agregar archivos de proyecto.

---
+++ Ciclo_For_números_múltiplos_5/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+int rango = 0, sumatotal = 0;
+Console.WriteLine("Imgrese el rango a evaluar: ");
+rango = Int32.Parse(Console.ReadLine());
+string mensaje = "";
+for (int i = 0; i <= rango; i++)
+{
+    if (i % 5 ==0)
+    {
+        Console.WriteLine(i);
+    }
+
+}
